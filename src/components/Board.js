5afcd56cf030e4af81bc5e95ef5001fa0e17fa9d
yushchenko yuliia <yushchenko.yu@gmail.com>
2021-03-30 00:52:50 +0300

    Latest version. The app contains: field for play, NEXT / WINNER / DRAW pointer, game score, NEW GAME and RESET buttons

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import CalculateWinner from './CalculateWinner';
-import Status from './Status';
 
-const initialStateBoard = Array(9).fill(null)
-const initialStateIsX = true
+const initialStateBoard = Array(9).fill(null);
+const initialStateIsX = true;
 const initialStateX = 0
 const initialStateO = 0
+
 
 function Board() {
     const [board, setBoard] = useState(initialStateBoard)
@@ -14,13 +14,10 @@ function Board() {
     const [counterX, setCounterX] = useState(initialStateX);
     const [counterO, setCounterO] = useState(initialStateO)
 
-    const gameFinish = Status(board);
-    console.log("status -" + gameStatus)
+    const gameFinish = board.every((el) => el !== null);
 
 
-
-
-    const upDateGame = (i, char) => {
+    const upDateGame = (i) => {
         const newBoard = [...board]
         if (winner || newBoard[i]) return null
         newBoard[i] = isX ? 'X' : 'O'
@@ -38,33 +35,14 @@ function Board() {
         }
     }
 
-
-
-    // СЧИТАЕТ ТОЛЬКО ДО РЕЗУЛЬТАТА 1:1
-    const score = () => {
-        if (winner == 'X') { counterX =+ 1
+    useEffect(() => {
+        if (winner == 'X') { 
+            setCounterX(counterX + 1);
+        } else if (winner == 'O') { 
+            setCounterO(counterO + 1);
         }
-        else if (winner == 'O') {counterO =+ 1
-        }
+    }, [winner])
 
-        return (
-            <span>{counterX} : {counterO}</span>
-        )
-
-    }    
-    // const score = () => { 
-    //     if (winner) { 
-    //         if (winner == 'X') { 
-    //             let newX = counterX + 1
-    //             return setCounterX(newX)
-    //         } else if (winner == 'O'){ 
-    //             let newO = counterO + 1
-    //             return setCounterO(newO)
-    //         } 
-    //     return (
-    //         <span>{counterX} : {counterO}</span>
-    //     )}
-    // }
 
     const reset = () => {
         return (
@@ -78,39 +56,30 @@ function Board() {
     return (
     <div className = 'vertically'> 
         {status()}
-        <div className = 'horizon'>
-            <div>
-                <span className = 'first player'>Player X</span>
-                {/* ХОЧУ ВЫВОД ИСТОРИИ ХОДОВ  */}
-            </div>
-
-            <div className = 'table'>
-                <biv className = 'line'>
-                    <div className = 'cell' onClick = { () => upDateGame(0) } >{board[0]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(1) } >{board[1]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(2) } >{board[2]}</div>
-                </biv>
-                <biv className = 'line'>
-                    <div className = 'cell' onClick = { () => upDateGame(3) } >{board[3]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(4) } >{board[4]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(5) } >{board[5]}</div>
-                </biv>
-                <biv className = 'line'>
-                    <div className = 'cell' onClick = { () => upDateGame(6) } >{board[6]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(7) } >{board[7]}</div>
-                    <div className = 'cell' onClick = { () => upDateGame(8) } >{board[8]}</div>
-                </biv>
-            </div>
-            <div>
-                <span className = 'first player'>Player O</span>
-                {/* ХОЧУ ВЫВОД ИСТОРИИ ХОДОВ  */}
-            </div>
-
+        <div className = 'table'>
+            <biv className = 'line'>
+                <div className = 'cell' onClick = { () => upDateGame(0) } >{board[0]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(1) } >{board[1]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(2) } >{board[2]}</div>
+            </biv>
+            <biv className = 'line'>
+                <div className = 'cell' onClick = { () => upDateGame(3) } >{board[3]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(4) } >{board[4]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(5) } >{board[5]}</div>
+            </biv>
+            <biv className = 'line'>
+                <div className = 'cell' onClick = { () => upDateGame(6) } >{board[6]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(7) } >{board[7]}</div>
+                <div className = 'cell' onClick = { () => upDateGame(8) } >{board[8]}</div>
+            </biv>
         </div>
-        {score()}
-        <button className = 'btn' onClick = { () => {setBoard(initialStateBoard)}}> START NEW GAME </button>
-        <button className = 'btn' onClick = { () => reset() }> RESET </button>
-    </ div > )
+        <div className = 'functional'>
+            <span>{counterX} : {counterO}</span>
+            <button className = 'btn' onClick = { () => {setBoard(initialStateBoard)}}> START NEW GAME </button>
+            <button className = 'btn' onClick = { () => reset() }> RESET </button>
+        </div>
+    </ div > 
+    )
 }
 
 export default Board
